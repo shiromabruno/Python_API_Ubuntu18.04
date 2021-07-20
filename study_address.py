1005e9012b1ac775a07a86754b546ca48a1b1dcb
Bruno Shiroma <bruno_shiroma@hotmail.com>
2021-07-20 16:01:51 -0300

    atribute from a differente class

--- study_address.py
+++ study_address.py
@@ -1,11 +1,23 @@
 class Address:
 
+    # def __init__(self, street='', number='', state='', country=''):
+    #     self.street = street
+    #     self.number = number
+    #     self.state = state
+    #     self.country = country
+
     def __init__(self, street, number, state, country):
         self.street = street
         self.number = number
         self.state = state
         self.country = country
     
+    # def add_values(self, street, number, state, country):
+    #     self.street = street
+    #     self.number = number
+    #     self.state = state
+    #     self.country = country
+
     def print_address(self):
         print("Street: " + self.street,
         "Number: " + str(self.number),
@@ -23,10 +35,11 @@ class Address:
         return self.country
 
     
-   
+#address1 = Address("Street 1", 123, "Sao Paulo", "Brazil")   
+#address2 = Address("Street Urban Field", 7421, "New York", "America")   
+# address1.add_values("Street 1", 123, "Sao Paulo", "Brazil")
+# address2.add_values("Street Urban Field", 7421, "New York", "America")
 
-address1 = Address("Street 1", 123, "Sao Paulo", "Brazil")
-address2 = Address("Street Urban Field", 7421, "New York", "America")
-
-print(address1.get_state())
-# address2.print_address()
+# #print(address1.get_state())
+#address1.print_address()
+#address2.print_address()
--- study_person.py
+++ study_person.py
@@ -1,13 +1,17 @@
-import study_address
+from study_address import Address
 
 class Person:
-    def __init__(self, name, age, birth, email, PRECISO PASSAR OBJETO ADDRESS AKI):
+    def __init__(self, name, age, birth, email):
         self.name = name
         self.age = age
         self.birth = birth
         self.email = email
-        self.place = OBJETO ADDRESS
-    
+        self.place = []
+
+    def add_address(self, street, number, state, country):
+        endereco = Address(street, number, state, country)
+        self.place.append(endereco)
+
     def change_name(self, new_name):
         self.name = new_name
 
@@ -15,17 +19,23 @@ class Person:
         print("Name: " + self.name,
         "Age: " + str(self.age),
         "Birth: " + self.birth,
-        "Email: " + self.email,
-        "Place: " + self.place
+        "Email: " + self.email
+        #"Place: " + list(self.place)
         )
 
-address3 = study_address.Address("Rua Amarildo", 1290, "Rio de Janeiro", "Brazil")
-address4 = study_address.Address("Satoshi Toori", 25, "Okinawa", "Japan")
-person1 = Person("Pessoa 1", 45, '1990-05-23', "person1@email.com", address3)
-person2 = Person("Pessoa 2", 21, '2003-11-03', "person2@email.com", address4)
+    def print_address_person(self):
+        for end in self.place:
+            print (end.print_address())
+        
+
+
+person1 = Person("Pessoa 1", 45, '1990-05-23', "person1@email.com")
+#person2 = Person("Pessoa 2", 21, '2003-11-03', "person2@email.com")
+person1.add_address("Satoshi Toori", 25, "Okinawa", "Japan")
 
 person1.print_person()
-person2.print_person()
+person1.print_address_person()
+#person2.print_person()
 
 
 # if __name__ == "__main__":
